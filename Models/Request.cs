418bafd7e9148c016c02e95098fd5b1939d7ec7d
Entropyi <jemo1x503@gmail.com>
2024-12-01 20:39:18 +0300

    Test

--- Models/Request.cs
+++ Models/Request.cs
@@ -1,57 +1,69 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace feedback.Models;
 
-[Index(nameof(Email), IsUnique = true)]
 public class Request
 {
- 
-    
+    public enum AgeGroups
+    {
+        Lessthan25,
+        أقلمن25
+    }
+
+    [Required(ErrorMessage = "Required")] public AgeGroups AgeGroup { get; set; }
+
+
     public int Id { get; set; }
-    
+
     [Required(ErrorMessage = "Required")]
     [MinLength(10, ErrorMessage = "InvalidNameLengthMin")]
     [MaxLength(29, ErrorMessage = "InvalidNameLengthMax")]
     public string FullNameInArabic { get; set; }
-    
+
     [Required(ErrorMessage = "Required")]
     [MinLength(10, ErrorMessage = "InvalidNameLengthMin")]
     [MaxLength(29, ErrorMessage = "InvalidNameLengthMax")]
     public string FullNameInEnglish { get; set; }
-    
+
     [Required(ErrorMessage = "Required")]
     [RegularExpression(@"^05[0-9]{8}$", ErrorMessage = "InvalidPhoneNumber")]
     [MinLength(10, ErrorMessage = "PhoneNumberLength")]
     [MaxLength(10, ErrorMessage = "PhoneNumberLength")]
     public string PhoneNumber { get; set; }
-    
+
     [Required(ErrorMessage = "Required")]
-    [EmailAddress (ErrorMessage = "InvalidEmail")]
+    [EmailAddress(ErrorMessage = "InvalidEmail")]
 
     public string Email { get; set; }
-    
-    [Required(ErrorMessage = "Required")]
-    public string Nationality { get; set; }
-    
-    
-    [Required(ErrorMessage = "Required")]
-    public DateOnly? DateOfBirth { get; set; }
-   
+
+    [Required(ErrorMessage = "Required")] public string Nationality { get; set; }
+
+
+    [Required(ErrorMessage = "Required")] public DateOnly? DateOfBirth { get; set; }
+
     [Required(ErrorMessage = "Required")]
     [MinLength(5, ErrorMessage = "InvalidIdeaNameLengthMin")]
     [MaxLength(15, ErrorMessage = "InvalidIdeaNameLengthMax")]
-    public string IdeaName { get; set; }
-    
+    public string? IdeaName { get; set; }
+
     [Required(ErrorMessage = "Required")]
     [MinLength(5, ErrorMessage = "InvalidIdeaDescriptionLenghtMin")]
-    public string IdeaDescription { get; set; }
-    
+    public string? IdeaDescription { get; set; }
+
     [Required(ErrorMessage = "Required")]
     [MinLength(5, ErrorMessage = "InvalidPhoneNumberLengthMin")]
     [MaxLength(25, ErrorMessage = "InvalidPhoneNumberLengthMax")]
-    public string TeamName { get; set; }
-    
+    public string? TeamName { get; set; }
+
     public DateTime CreationDate { get; set; } = DateTime.Now;
 
+    [BindProperty] public string? Ge { get; init; } // Removed the semicolon here
+
+// Property to return an array of validation error codes
+    public string[] Ges => new[]
+    {
+        "Required", "InvalidPhoneNumberLengthMax", "InvalidIdeaNameLengthMin"
+    };
 }
--- obj/Debug/net8.0/feedback.AssemblyInfo.cs
+++ obj/Debug/net8.0/feedback.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("feedback")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a5c1e62f5a652ead655ad4cae6a05a41e339fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557f99a553ce03fcfccde0217e92140d1389801d")]
 [assembly: System.Reflection.AssemblyProductAttribute("feedback")]
 [assembly: System.Reflection.AssemblyTitleAttribute("feedback")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
